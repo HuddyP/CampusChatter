d2cc11a9da9d9e249c527438f8540a1e01ab7d49
HuddyP <hudsonrp11@gmail.com>
2024-03-17 17:02:42 -0500

    added Firebase support and ChatterBox feature

---
+++ src/ChatterBox.js
@@ -0,0 +1,57 @@
+import { Avatar, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import db from "./firebase";
+import "./ChatterBox.css";
+
+function ChatterBox() {
+  const [ChatterMessage, setChatterMessage] = useState("");
+  const [ChatterImage, setChatterImage] = useState("");
+
+  const sendChatter = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      username: "HuddyP",
+      displayName: 'Hudson "HuddyP" Pfenning',
+      avatar: " ",
+      verified: true,
+      text: ChatterMessage,
+      image: ChatterImage,
+    });
+
+    setChatterMessage("");
+    setChatterImage("");
+  };
+
+  return (
+    <div className="ChatterBox">
+      <form>
+        <div className="ChatterBox__input">
+          <Avatar src=" " />
+          <input
+            value={ChatterMessage}
+            onChange={(e) => setChatterMessage(e.target.value)}
+            placeholder="What's the Chatter?"
+            type="text"
+          />
+        </div>
+        <input
+          placeholder="Optional: Enter Image URL"
+          value={ChatterImage}
+          onChange={(e) => setChatterImage(e.target.value)}
+          type="text"
+          className="ChatterBox__imageInput"
+        />
+        <Button
+          onClick={sendChatter}
+          type="submit"
+          className="ChatterBox__button"
+        >
+          Start Some Chatter
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default ChatterBox;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6XhLmAi__dCJPwqTgvs7HVfeh4eQfXVw",
+  authDomain: "campuschatter-94994.firebaseapp.com",
+  projectId: "campuschatter-94994",
+  storageBucket: "campuschatter-94994.appspot.com",
+  messagingSenderId: "136617986555",
+  appId: "1:136617986555:web:e2dcd107ad44bd9526235e",
+};
+
+const app = initializeApp(firebaseConfig);
