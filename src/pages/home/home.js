47a17e1e322cf080cb2d36d94f4f807a208a7184
HuddyP <hudsonrp11@gmail.com>
2024-04-01 20:23:26 -0500

    added calendar (is currently on home page, needs changed to its own page)

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,14 +3,16 @@ import "../../App.css";
 import Feed from "../../Feed";
 import Sidebar from "../../sidebar";
 import Widgets from "../../Widgets";
+import EventCalendar from "../events/calendar.js";
 
 function Home() {
-    return (
-      <div className="app">
-        <Sidebar />
-        <Widgets />
-      </div>
-    );
-  }
-  
-  export default Home;
+  return (
+    <div className="app">
+      <Sidebar />
+      <EventCalendar />
+      <Widgets />
+    </div>
+  );
+}
+
+export default Home;
